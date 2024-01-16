ef41cdef5fcf0eec822422ba9770e187ebab992a
joshua-303 <147515580+joshua-303@users.noreply.github.com>
2024-01-16 11:34:38 -0500

    Add files via upload

---
+++ Prog1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void main() {
+	system("cls");
+
+	cout << "		<<< Game Instructions >>>" << endl;
+	cout << "								 " << endl;
+	cout << "1. Use the 'left', 'down' and" << endl;
+	cout << "	'right' arrow keys on your keyboard" << endl;
+	cout << "	to hit the 'coin', 'green note' and" << endl;
+	cout << "	'red note' respectively." << endl;
+	cout << "							" << endl;
+	cout << "2. You MUST hit the keys the same	" << endl;
+	cout << "	number of times as the number of" << endl;
+	cout << "	icons appear." << endl;
+	cout << "	<--	 |	-->" << endl;
+	cout << "		 v	   " << endl;
+	cout << "			   " << endl;
+
+	system("pause");
+}
