5d1e04ba19a3b9aeaaa1d036f46c25558dd5b545
unknown <cadorato@email.neit.edu>
2024-01-21 16:22:28 -0500

    Quest 2 pt 1

---
+++ prog3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void main() {
+    string team = "Red";
+    int wepControl = 40;
+    int caps = 2;
+    int owns = 0;
+    int efficiency = 50;
+    cout << team << " Team\n\nWeapon Control: " << wepControl << "%\nCaptures: " << caps << "\nOwnages: " << owns << "\nEfficiency: " << efficiency << "%" << endl;
+    system("pause");
+}
