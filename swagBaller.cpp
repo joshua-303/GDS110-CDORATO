0092598e2d2cec49ef56ef70db91dd41b59ca118
unknown <cadorato@email.neit.edu>
2024-01-27 00:06:58 -0500

    What

--- swagBaller.cpp
+++ swagBaller.cpp
@@ -7,8 +7,8 @@ void main() {
     srand(time(NULL));
 
     int compRand = rand() % 100 + 1;
-    int playerInput;
     int attemptNum = 0;
+    int playerInput;
 
     for(attemptNum; attemptNum <= 4; attemptNum++) {
         cout << "Guess a number between 1 and 100! You've currently used " << attemptNum << " attempts! ";
@@ -22,6 +22,7 @@ void main() {
         } else if (playerInput < compRand) {
             cout << "Your guess was too low, go higher." << endl;
         }
+        system("pause");
     }
 
     cout << "You've used all 5 attempts, which means that you've lost. Too bad! The number was " << compRand << "!"  << endl;
