6bf9b44436bd2feaaabc36c9b547876940f7d080
unknown <cadorato@email.neit.edu>
2024-02-23 22:12:25 -0500

    Hangman quest

---
+++ hangman.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+string words[] = {"smile", "niceness", "bahamut", "dollar", "spongebob"};
+string hangman[] = {
+        "+---------+",
+        "|         O",
+        "|        /|\\",
+        "|        / \\",
+        "|          ",
+        "+===       "
+    };
+
+int main() {
+    srand(time(NULL));
+    int randNum = rand() % (sizeof(words) / sizeof(string));
+    int attempts = 0;
+    string secret = words[randNum];
+    string tempSecret = secret;
+    string publicWord = string(secret.length(), '_');
+    cout << publicWord << endl;
+
+    while(publicWord.compare(secret) != 0) {
+        char playerGuess;
+        cout << "Guess the word! ";
+        cin >> playerGuess;
+
+        string guess(1, playerGuess);
+        bool charIsFound = false;
+
+        //checks tempsecret for every instance of a successful guess within the string
+        for(int foundChar = 0; foundChar != -1 && playerGuess != '*';) {
+                foundChar = tempSecret.find(guess);
+                if (foundChar != -1) {
+                    charIsFound = true;
+                    publicWord.replace(foundChar, 1, guess);
+                    tempSecret.replace(foundChar, 1 , "*");
+                }
+        }
+
+        cout << publicWord << endl;
+
+        if (charIsFound == false) {
+            attempts++;
+
+            for(int i = 1; i <= attempts; i++) {
+                cout << hangman[i-1] << endl;
+            }
+        }
+
+        if (attempts >= 6) {
+            cout << "Unfortunately, you've lost." << endl;
+            system("pause");
+            exit(0);
+        }
+    }
+    
+    cout << "You've won! Congratulations! It took you " << attempts << " attempts to guess the word, '" << secret << "'." << endl;
+    system("pause");
+    exit(0);
+}
